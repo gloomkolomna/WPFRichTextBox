df3593919f1bd523da2075b882983eb403309fa1
belovolov <belovolov@ascon.ru>
2017-09-22 10:08:35 +0300

    Test

--- WPFRichTextBox/WPFRichTextBox/MainWindow.xaml.cs
+++ WPFRichTextBox/WPFRichTextBox/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,61 @@ namespace WPFRichTextBox
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RtfEditOnClick(object sender, RoutedEventArgs e)
+        {
+            var rtfDialog = new RtfView();
+            rtfDialog.SetText(Text);
+            rtfDialog.ShowDialog();
+            if (rtfDialog.IsOk)
+            {
+                LoadRtfFromString(rtfDialog.Text);
+                Text = rtfDialog.Text;
+            }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text",
+            typeof(string),
+            typeof(MainWindow),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                TextPropertyChangedCallback));
+
+        private static void TextPropertyChangedCallback(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var newValue = (string)dependencyPropertyChangedEventArgs.NewValue;
+            var rtfView = (MainWindow)dependencyObject;
+            rtfView.LoadRtfFromString(newValue);
+        }
+
+        private void LoadRtfFromString(string rtfString)
+        {
+            var range = new TextRange(RtbEditor.Document.ContentStart, RtbEditor.Document.ContentEnd);
+            try
+            {
+                using (var stream = new MemoryStream())
+                {
+                    using (var rtfStreamWriter = new StreamWriter(stream))
+                    {
+                        rtfStreamWriter.Write(rtfString);
+                        rtfStreamWriter.Flush();
+                        stream.Seek(0, SeekOrigin.Begin);
+
+                        range.Load(stream, DataFormats.Rtf);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Dispatcher.BeginInvoke(new Action(() => MessageBox.Show(e.Message)));
+            }
         }
     }
 }
---
+++ WPFRichTextBox/WPFRichTextBox/RtfView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFRichTextBox
+{
+    /// <summary>
+    /// Interaction logic for RtfView.xaml
+    /// </summary>
+    public partial class RtfView : Window
+    {
+        public RtfView()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveRtfOnClick(object sender, RoutedEventArgs e)
+        {
+            string rtfFromRtb;
+            var range = new TextRange(rtfEditor.Document.ContentStart, rtfEditor.Document.ContentEnd);
+
+            using (var ms = new MemoryStream())
+            {
+                range.Save(ms, DataFormats.Rtf);
+                ms.Seek(0, SeekOrigin.Begin);
+                using (var sr = new StreamReader(ms))
+                {
+                    rtfFromRtb = sr.ReadToEnd();
+                }
+            }
+
+            Text = rtfFromRtb;
+            IsOk = true;
+            Close();
+        }
+
+        public void SetText(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return;
+
+            var byteArray = Encoding.UTF8.GetBytes(text);
+            using (var stream = new MemoryStream(byteArray))
+            {
+                var range = new TextRange(rtfEditor.Document.ContentStart, rtfEditor.Document.ContentEnd);
+                range.Load(stream, System.Windows.DataFormats.Rtf);
+            }
+
+            rtfEditor.CaretPosition = rtfEditor.Document.ContentEnd;
+            rtfEditor.ScrollToEnd();
+        }
+
+        public string Text { get; set; }
+
+        public bool IsOk { get; set; }
+    }
+}
